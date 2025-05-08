dcbaad29ad9b9774e97305517371a3d4b4486054
covy <junni802@naver.com>
2025-05-08 23:35:00 +0900

    feat: add lotto-service

---
+++ src/test/java/covy/lotto/service/LottoServiceTest.java
@@ -0,0 +1,17 @@
+package covy.lotto.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoServiceTest {
+
+  @Autowired
+  private LottoService service;
+
+  @Test
+  void lottoservice() {
+    service.lottoservice(5);
+  }
+}
